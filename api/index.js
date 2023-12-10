a5654fe11cdc752bb3e34690f66aa8ad44d61bfc
Vitor Moutim <moutimg@gmail.com>
2023-12-10 02:00:56 -0300

    try deploy

--- api/index.js
+++ api/index.js
@@ -1,30 +1,3 @@
-const express = require('express');
+import app from '../src/index.js';
 
-const cors = require('cors');
-
-require('express-async-errors');
-
-const app = express();
-
-const middlewares = require('../src/middlewares');
-
-app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', '*');
-  app.use(cors());
-  next();
-});
-
-app.use(require('../src/routes/index'));
-
-app.use(middlewares.errorHandler);
-
-app.get("/", (req, res) => {
-    res.send("Express on Vercel");
-  });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+export default app;
